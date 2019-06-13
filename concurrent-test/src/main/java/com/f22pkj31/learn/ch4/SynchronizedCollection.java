d4117162249d3539d04269bcf1b91a0248492249
bo.fan <bo.fan@ly.com>
2019-06-13 15:13:06 +0800

    Joda，Collections.synchronized简单使用

---
+++ concurrent-test/src/main/java/com/f22pkj31/learn/ch4/SynchronizedCollection.java
@@ -0,0 +1,28 @@
+package com.f22pkj31.learn.ch4;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * <p>
+ * <p>
+ * 修改历史:											<br>
+ * 修改日期    		修改人员   	版本	 		修改内容<br>
+ * -------------------------------------------------<br>
+ * 2019-06-13 11:32  bo.fan   1.0    	初始化创建<br>
+ * </p>
+ *
+ * @author bo.fan
+ * @version 1.0
+ * @since JDK1.7
+ */
+public class SynchronizedCollection {
+
+    public static void main(String[] args) {
+
+        List<String> strings = new ArrayList<String>();
+        //装饰者模式
+        strings = Collections.synchronizedList(strings);
+    }
+}
---
+++ joda-test/src/main/java/com/f22pkj31/learn/JodaTest.java
@@ -0,0 +1,18 @@
+package com.f22pkj31.learn;
+
+/**
+ * <p>
+ * <p>
+ * 修改历史:											<br>
+ * 修改日期    		修改人员   	版本	 		修改内容<br>
+ * -------------------------------------------------<br>
+ * 2019-06-11 14:58  bo.fan   1.0    	初始化创建<br>
+ * </p>
+ *
+ * @author bo.fan
+ * @version 1.0
+ * @since JDK1.7
+ */
+public class JodaTest {
+
+}
